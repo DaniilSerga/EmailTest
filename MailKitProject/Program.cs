6f8e6d6e0733b67a01692345ef229bc3e11e2d91
daniilsergx <daniil.serga@gmail.com>
2022-10-10 19:14:13 +0300

    refactored code

--- MailKitProject/Program.cs
+++ MailKitProject/Program.cs
@@ -6,39 +6,40 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //string sValue = ConfigurationManager.AppSettings["sender"];
-
-        //Console.WriteLine(sValue);
-
         string name = "Иванов Иван Иванович";
         string address = "г. Солигорск, ул. Заморская, д.165";
         string email = "ivenov@mail.ru";
         string appealContaining = "Лайк за продукцию.";
         string reciever = "Генеральному директору БрестМясоМолПрома";
 
-        MimeMessage message = new()
-        {
-            From = new MailboxAddress("Моя компания", ConfigurationManager.AppSettings["sender"]),
-            To = new MailboxAddress(name, ConfigurationManager.AppSettings["reciever"]),
-            Subject = "Электронное обращение",
-            Body = $"Отправитель: {name}\n" +
-        $"Адрес: {address}\n" +
-        $"Email: {email}\n\n" +
-        $"Получатель: {reciever}\n\n" +
-        $"Текст обращения:\n{appealContaining}"
-        };
-        message.From.Add(new MailboxAddress("Моя компания", ConfigurationManager.AppSettings["sender"])); //отправитель сообщения
-        message.To.Add(new MailboxAddress(name, ConfigurationManager.AppSettings["reciever"])); //адресат сообщения
-        message.Subject = "Электронное обращение"; //тема сообщения
-        message.Body = new BodyBuilder() { TextBody = appealContaining }.ToMessageBody();
+        MimeMessage message = new();
 
-        using (MailKit.Net.Smtp.SmtpClient client = new())
-        {
-            client.Connect(ConfigurationManager.AppSettings["host"], 587, true); //либо использум порт 465
-            client.Authenticate(ConfigurationManager.AppSettings["sender"], ConfigurationManager.AppSettings["password"]); //логин-пароль от аккаунта
-            client.Send(message);
+        // Sender
+        message.From.Add(new MailboxAddress("Моя компания", ConfigurationManager.AppSettings["sender"]));
 
-            client.Disconnect(true);
-        }
+        // Reciever
+        message.To.Add(new MailboxAddress(name, ConfigurationManager.AppSettings["reciever"]));
+
+        // Appeal
+        message.Body = new BodyBuilder()
+        {
+            TextBody = $"Отправитель: {name}\n" +
+                       $"Адрес: {address}\n" +
+                       $"Email: {email}\n\n" +
+                       $"Получатель: {reciever}\n\n" +
+                       $"Текст обращения:\n{appealContaining}"
+        }.ToMessageBody();
+
+        // MessageTheme
+        message.Subject = $"Электронное обращение от {name}";
+
+        // IDisposable
+        using MailKit.Net.Smtp.SmtpClient client = new();
+
+        client.Connect(ConfigurationManager.AppSettings["host"], 587, true); //либо использум порт 465
+        client.Authenticate(ConfigurationManager.AppSettings["sender"], ConfigurationManager.AppSettings["password"]); //логин-пароль от аккаунта
+        client.Send(message);
+
+        client.Disconnect(true);
     }
 }
